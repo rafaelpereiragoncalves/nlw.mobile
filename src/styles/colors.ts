139d93384112ac264d2eae4b4e4841b17e09fc3c
Rafael Pereira Gonçalves <rafael.goncalves@tinnova.com.br>
2025-08-06 08:42:50 -0300

    Adds a new project structure with components and features for coupon management and store location. Includes integration with the Expo icon and font library, as well as adjustments to TypeScript dependencies and configuration.

---
+++ src/styles/colors.ts
@@ -0,0 +1,32 @@
+export const colors = {
+  gray: {
+    100: "#FCFDFE",
+    200: "#E1EBF4",
+    300: "#C4D0DB",
+    400: "#73808C",
+    500: "#45525F",
+    600: "#1A1F24",
+  },
+
+  green: {
+    soft: "#E9F3EF",
+    light: "#3B9B62",
+    base: "#257F49",
+    dark: "#052914",
+  },
+
+  red: {
+    light: "#FDEDED",
+    base: "#F94144",
+  },
+
+  pink: {
+    100: "#FFF5F8", // Rosa claro, quase branco
+    200: "#FCDDE1", // Rosa muito suave
+    300: "#F8A3C2", // Rosa pastel
+    400: "#F06A9C", // Rosa vibrante
+    500: "#E63973", // Rosa intenso
+    600: "#B2245C", // Rosa escuro
+    700: "#7A163E", // Rosa muito escuro
+  },
+}
